50ba3ac4d7bd158232e206a1d66adbb9982924d7
Albert <albertdang8@gmail.com>
2023-05-17 16:24:41 -0700

    trying to fix an error

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,28 @@
 import { useState } from 'react'
+import { BrowserRouter, Routes, Route} from 'react-router-dom'
 import './App.css'
 import Header from './components/Header/Header'
 import Footer from './components/Footer/Footer'
+import About from './pages/About/About'
+import Episodes from './pages/Episodes/Episodes'
 import Homepage from './pages/Homepage/Homepage'
 
 function App() {
 
   return (
-    <>
+    <BrowserRouter>
       <Header />
-      <Homepage />
+
+      <Routes>
+        <Route path='/' element={<Homepage />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/episodes' element={<Episodes />} />
+      </Routes>
+      
+
       <Footer />
-    </>
+    <BrowserRouter/>
   )
 }
 
-export default App
+export default App;
