b3e73c8749e5671858ee9d41345d6f1949c3be97
Albert <albertdang8@gmail.com>
2023-05-31 00:02:14 -0700

    dynamic dropdown for episode page completed

--- src/pages/Episodes/Episodes.jsx
+++ src/pages/Episodes/Episodes.jsx
@@ -1,8 +1,35 @@
-import React from 'react'
+import React, { useEffect, useState }from 'react'
+import axios from 'axios'
 
 function Episodes() {
+  const [options, setOptions] = useState([]);
+
+  useEffect(() => {
+    axios.get(`https://rickandmortyapi.com/api/episode`)
+      .then(res => {
+        console.log(res.data.info.count);
+        const newOptions = [];
+
+        for(let i = 1; i <= res.data.info.count; i++) {
+          newOptions.push(i);
+        }
+        setOptions(newOptions);
+      })
+      .catch(err => console.log("Error: ", err));
+  }, []);
+
+
   return (
-    <div>Episodes</div>
+    <div className='episodes-container'>
+      <div>
+        <label htmlFor="select-episode">Select an episode: </label>
+        <select id='select-episode'>
+          {
+            options.map(num => <option key={num}>{`Episode ${num}`}</option>)
+          }
+        </select>
+      </div>
+    </div>
   )
 }
 
