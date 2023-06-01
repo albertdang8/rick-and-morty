665c9aabf0ecd87742590ea4c012225d23faee28
Albert <albertdang8@gmail.com>
2023-06-01 00:50:44 -0700

    a very confusing chain of events on the episodes page. my uncalloused brain is blistering

--- src/pages/Episodes/Episodes.jsx
+++ src/pages/Episodes/Episodes.jsx
@@ -1,36 +1,80 @@
-import React, { useEffect, useState }from 'react'
-import axios from 'axios'
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import CharacterCard from '../../components/CharacterCard/CharacterCard'
+import './Episodes.css'
 
 function Episodes() {
-  const [options, setOptions] = useState([]);
+  const [options, setOptions] = useState([]); //generates select list
+  const [selectedOption, setSelectedOption] = useState(1); //dropdown value
+  const [selectedEpisode, setSelectedEpisode] = useState(); //complete episode information
+  const [characterList, setCharacterList] = useState([]); //store 'selectedEpisode's characters. sub-object. Promise.all() returns an array
+
+  const fetchEpisodeData = async () => {
+    try {
+      const res = await axios.get(
+        `https://rickandmortyapi.com/api/episode/${selectedOption}`
+      );
+      // console.log(res.data)
+      setSelectedEpisode(res.data);
+
+      const episodeCharacters = await Promise.all( //iterate through the character list given as endpoints
+        res.data.characters.map(url => {
+          return axios.get(url).then((res) => res.data);
+        })
+      );
+
+      setCharacterList(episodeCharacters);
+    } catch (err) {
+      console.log("Error: ", err);
+    }
+  };
 
   useEffect(() => {
-    axios.get(`https://rickandmortyapi.com/api/episode`)
-      .then(res => {
+    axios
+      .get(`https://rickandmortyapi.com/api/episode`)
+      .then((res) => {
         console.log(res.data.info.count);
         const newOptions = [];
 
-        for(let i = 1; i <= res.data.info.count; i++) {
+        for (let i = 1; i <= res.data.info.count; i++) {
           newOptions.push(i);
         }
         setOptions(newOptions);
       })
-      .catch(err => console.log("Error: ", err));
+      .catch((err) => console.log("Error: ", err));
   }, []);
 
+  useEffect(() => {
+    fetchEpisodeData()
+  }, [selectedOption]);
+
+  const handleSelectChange = (e) => {
+    setSelectedOption(e.target.value);
+  };
 
   return (
-    <div className='episodes-container'>
+    <div className="episodes-container">
       <div>
         <label htmlFor="select-episode">Select an episode: </label>
-        <select id='select-episode'>
-          {
-            options.map(num => <option key={num}>{`Episode ${num}`}</option>)
-          }
+        <select id="select-episode" onChange={handleSelectChange}>
+          {options.map((num) => (
+            <option key={num} value={num}>{`Episode ${num}`}</option>
+          ))}
         </select>
       </div>
+      <div>
+        <div className="episode-info">
+          <p>Episode Name: {selectedEpisode?.name}</p>
+          <p>Air date: {selectedEpisode?.air_date}</p>
+        </div>
+        <div className="character-container">
+          {
+            characterList.map(item => <CharacterCard character={item} key={item.id} />)
+          }
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Episodes
+export default Episodes;
