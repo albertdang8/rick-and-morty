b90e946685102df57571394defdab0c9d2b611b1
Albert Dang <98789245+albertdang8@users.noreply.github.com>
2023-06-06 15:22:50 -0700

    i think i'm done?

--- src/pages/Favorites/Favorites.jsx
+++ src/pages/Favorites/Favorites.jsx
@@ -1,4 +1,4 @@
-import React, {useContext} from 'react'
+import React, { useContext } from 'react'
 import CharacterCard from '../../components/CharacterCard/CharacterCard'
 import { FavoritesContext } from '../../contexts/FavoritesContext'
 import { ThemeContext } from '../../contexts/ThemeContext'
